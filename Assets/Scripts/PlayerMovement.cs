92c1b59378d4f6600aeb767d969e10a5b80d2904
Nannings @ Vaio DESKTOP-7JBVPDM\lokaal1 <nannings mrnannings@gmail.com>
2017-11-07 12:10:47 +0100

    basic player movement; crt shader;

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float movementSpeed = 10;
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        body.velocity = targetVelocity * movementSpeed;
+    }
+}
