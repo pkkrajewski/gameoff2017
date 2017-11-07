4fe66064fe5c4445ec10d17d5a10fd2e9a3d9053
Nannings @ Vaio DESKTOP-7JBVPDM\lokaal1 <nannings mrnannings@gmail.com>
2017-11-07 17:18:47 +0100

    starting rooms;

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private Transform target;
+
+    private void Update()
+    {
+        if (!target || target == null)
+            return;
+
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, newPos, 5 * Time.deltaTime);
+    }
+
+    public void SetTarget(Transform t)
+    {
+        target = t;
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    private bool hasEntered;
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = FindObjectOfType<Camera>();
+    }
+
+    private void Update()
+    {
+            
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!hasEntered && collision.name == "Player")
+        {
+            hasEntered = true;
+            cam.SetTarget(this.transform);
+        }
+    }
+}
