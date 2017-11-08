66a5014467ba69a1131b73261d87ed4f306b9a11
diablol1 <patryk.krajewski002@gmail.com>
2017-11-08 08:25:28 +0100

    Revert movement script to older

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float movementSpeed = 10;
-    private float rotationSpeed = 5;
     private Rigidbody2D body;
 
     private void Awake()
@@ -13,32 +12,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        transform.rotation = Quaternion.FromToRotation(Vector3.up, GetDirectionToMouse());
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
-    }
-
-    private void Move()
-    {
-        int upOrDown = 0; //-1 or 1
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-            upOrDown = 1;
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-            upOrDown = -1;
-
-        body.MovePosition((Vector2)transform.position + GetDirectionToMouse() * movementSpeed * upOrDown * Time.deltaTime);
-    }
-
-    private Vector2 GetDirectionToMouse()
-    {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePosition - (Vector2)transform.position;
-
-        direction.Normalize();
-
-        return direction;
+        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        body.velocity = targetVelocity * movementSpeed;
     }
 }
