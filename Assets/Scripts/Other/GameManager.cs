3547e1074f4dec22d96a177d90dbfd98b5299c95
Nannings @ Vaio DESKTOP-7JBVPDM\lokaal1 <mrnannings@gmail.com>
2017-11-10 16:20:30 +0100

    change script folder structure; new font; gameover overlay;

---
+++ Assets/Scripts/Other/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject hudPanel;
+    private GameObject gameoverPanel;
+
+    private void Awake()
+    {
+        hudPanel = GameObject.Find("hud");
+        gameoverPanel = GameObject.Find("GameOver");
+    }
+
+    private void Start()
+    {
+        gameoverPanel.SetActive(false);
+    }
+}
