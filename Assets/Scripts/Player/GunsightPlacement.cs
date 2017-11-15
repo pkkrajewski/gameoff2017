7401a10f3fdac973843137fba5424921cab043ae
diablol1 <patryk.krajewski002@gmail.com>
2017-11-15 22:16:50 +0100

    Add more precision for player shooting

--- Assets/Scripts/Player/GunsightPlacement.cs
+++ Assets/Scripts/Player/GunsightPlacement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class GunsightPlacement : MonoBehaviour
 {
+    Vector2 cursorOffset = new Vector2(-0.22f, 0);
+
     void Awake()
     {
         Cursor.visible = false;
@@ -10,6 +12,8 @@ public class GunsightPlacement : MonoBehaviour
 	void Update ()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition += cursorOffset;
+
         transform.localPosition = mousePosition;
     }
 }
