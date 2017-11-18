91bd194e7630d7b97c8cf13a7b164bb80e9bbff8
Nannings @ Vaio DESKTOP-7JBVPDM\lokaal1 <mrnannings@gmail.com>
2017-11-18 12:43:14 +0100

    music manager; 4 music tracks;

---
+++ Assets/Scripts/Other/MusicManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance = null;
+    public AudioClip[] musicTracks;
+
+    private AudioSource audioSource;
+    private int trackNumber = -1;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            trackNumber++;
+            if (trackNumber > musicTracks.Length - 1)
+                trackNumber = 0;
+            audioSource.clip = musicTracks[trackNumber];
+            audioSource.Play();
+        }   
+    }
+}
